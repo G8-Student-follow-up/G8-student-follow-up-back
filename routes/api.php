e23bc1d12b3f061a05a1a575642125b3a5311960
phorn-ya <phornya26@gmail.com>
2026-07-08 09:29:17 +0700

    feature user role I have already done.

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,24 @@
 <?php
 
 use App\Http\Controllers\Api\StudentController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::apiResource('students', StudentController::class);
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::apiResource('students', StudentController::class);
+    Route::apiResource('users', UserController::class);
+});
+
+Route::middleware(['auth:sanctum', 'trainer'])->group(function () {
+    Route::get('/students', [StudentController::class, 'index']);
+    Route::get('/students/{student}', [StudentController::class, 'show']);
+    Route::put('/students/{student}', [StudentController::class, 'update']);
 });
