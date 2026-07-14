a6b35ccaeb24529166532d06697305fe8d98fb58
S2-svg <savin.phuong@student.passerellesnumeriques.org>
2026-07-14 08:15:29 +0700

    feat: update backend APIs.

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,9 @@ return [
 
     'allowed_origins' => [
         'http://localhost:5173',
-        'http://127.0.0.1:5173'
+        'http://127.0.0.1:5173',
+        'http://localhost:5174',
+        'http://127.0.0.1:5174',
 
         ],
 
